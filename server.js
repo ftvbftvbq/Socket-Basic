e89da354090c90401820f0a0c0ae4428a720ee6c
ftvbftvbq <ftvbftvbq@163.com>
2016-04-12 13:50:26 +0800

    add @currentUser part

--- server.js
+++ server.js
@@ -9,6 +9,28 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+function sendCurrentUsers (socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+	if (typeof info === 'undefined') {
+	return;
+	}
+
+	Object.keys(clientInfo).forEach(function (socketId) {
+		var userInfo = clientInfo[socketId];
+		if (info.room === userInfo.room) {
+			users.push(userInfo.name);
+		}
+	});
+
+	socket.emit('message', {
+			name: '系统登录时间',
+			text: '目前的用户有：' + users.join(', '),
+			timestamp: moment().valueOf()
+	});
+}
+
+
 io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
 
@@ -39,11 +61,14 @@ io.on('connection', function (socket) {
 
 	socket.on('message', function (message) {
 		console.log('Message received:' + message.text);
-
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
 		// socket.broadcast.emit('message', message);
 		message.timestamp = moment().valueOf();
 		// io.emit('message', message);
 		io.to(clientInfo[socket.id].room).emit('message', message);
+		}
 	});
 
 	socket.emit('message',  {
